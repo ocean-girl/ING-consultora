41ba13abb0011b2629632ba69d17ee634b2d4727
ocean-girl <vpdamill@gmail.com>
2023-09-02 18:25:09 -0300

    agrego estilos al carousel. falta hacerlo responsive

--- src/components/pages/carousel/CarouselImage.js
+++ src/components/pages/carousel/CarouselImage.js
@@ -3,18 +3,18 @@ export const imagesCarousel = [
     id: "carousel-1",
     img: "../../../../public/carousel-1.jpg",
     description:
-      "En ING Consultora, nuestro compromiso firme, acción decidida y enfoque en resultados son la base de nuestro trabajo.",
+      "En ING Consultora, nuestro compromiso firme, \n acción decidida y enfoque en resultados\n son la base de nuestro trabajo.",
   },
   {
     id: "carousel-2",
     img: "../../../../public/carousel-2.jpg",
     description:
-      "Nos dedicamos a la mejora continua de sus procesos, proporcionando soluciones a medida, fusionando conocimiento local con una perspectiva global.",
+      "Nos dedicamos a la mejora continua de sus procesos,\n proporcionando soluciones a medida,\n fusionando conocimiento local con una perspectiva global.",
   },
   {
     id: "carousel-3",
     img: "../../../../public/carousel-3.jpg",
     description:
-      "Somos su socio estratégico a largo plazo, basado en la ética y la transparencia, comprometidos a llevar su empresa hacia el éxito sostenible",
+      "Somos su socio estratégico a largo plazo,\n basado en la ética y la transparencia,\n comprometidos a llevar su empresa hacia el éxito sostenible.",
   },
 ];
--- src/components/pages/carousel/Items.jsx
+++ src/components/pages/carousel/Items.jsx
@@ -1,33 +1,51 @@
 import { Paper } from "@mui/material";
+import "./Item.css";
 
 function Item({ item }) {
   return (
     <Paper
       sx={{
-        backgroundAttachment: "scroll",
-
+        backgroundRepeat: "repeat-x",
         backgroundPosition: "0 0",
-        backgroundSize: "contain",
         minHeight: "70vh",
         backgroundImage: `url(${item.img})`,
-        backgroundRepeat: "repeat-x",
+        backgroundAttachment: "fixed",
+        backgroundSize: "50% auto",
+        position: "relative",
       }}
     >
       <img
         src={item.img}
         alt={item.id}
         style={{
-          width: "100%",
+          maxWidth: "100%",
           height: "auto",
-          backgroundAttachment: "scroll",
-          backgroundPosition: "0 0",
-          backgroundSize: "contain",
-          backgroundRepeat: "repeat-x",
           display: "none",
         }}
       />
 
-      <p>{item.description}</p>
+      <div
+        className="tear-drop"
+        style={{
+          color: "white",
+          position: "absolute",
+          padding: "5%",
+          background: "rgba(0, 0, 0, 0.7)",
+          backgroundAttachment: "fixed",
+          backgroundSize: "2%",
+          minHeight: "70vh",
+          top: 0,
+          width: "50%",
+          left: "0%",
+          overflow: "hidden",
+          clipPath: "var(--circle-top-right-out)",
+          animation: "in-circle-swoop 5s forwards",
+          fontSize: "1.5 rem",
+          lineHeight: "1.5",
+        }}
+      >
+        {item.description}
+      </div>
     </Paper>
   );
 }
