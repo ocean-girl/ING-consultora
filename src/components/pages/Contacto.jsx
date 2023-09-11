93d7efa4862e9f9457e7d74700565bd8cc9826fe
LordNickus <nicolas.jontin@gmail.com>
2023-09-11 16:44:55 -0300

    contacto sin formato

--- src/components/pages/Contacto.jsx
+++ src/components/pages/Contacto.jsx
@@ -1,6 +1,7 @@
 import { Box, Button, TextField } from "@mui/material";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Title from "../common/title/Title";
+import emailjs from "@emailjs/browser";
 
 export default function Contacto() {
   const [email, setEmail] = useState("");
@@ -30,60 +31,100 @@ export default function Contacto() {
     });
   };
 
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_l1okmtb', 'template_v132pk8', form.current, '0xQ6z1Nf22IOfl5mj')
+      .then((result) => {
+          console.log(result.text);
+          form.current.reset();
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
     <div>
       <Box sx={{ boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.3)" }}>
-        <Title title={"CONTACTO"} />
-
-        <Box
-          component="form"
-          onSubmit={onSubmit}
-          autoComplete="off"
-          sx={{
+      <Title title={"CONTACTO"} />
+        <Box sx={{
             display: "grid",
             gridTemplateColumns: { sm: "1fr 1fr" },
             gap: 2,
             p: 4,
-          }}
-        >
-          <TextField
-            required
-            id="outlined-required"
-            label="Nombre y Apellido"
-            margin="normal"
-          />
-          <TextField
-            required
-            id="outlined-required"
-            label="Celular"
-            inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
-            margin="normal"
-          />
-          <TextField
-            label="Email"
-            variant="outlined"
-            id="email"
-            type="email"
-            required
-            margin="normal"
-            error={error.error}
-            helperText={error.message}
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-          />
-          <TextField
-            id="outlined-multiline-static"
-            label="Comentario"
-            multiline
-            rows={4}
-            margin="normal"
-          />
+          }}>
+          <form ref={form} onSubmit={sendEmail}>
+            <label>Nombre y Apellido</label>
+            <input type="text" name="user_name" required/>
+            
+            <label>Celular</label>
+            <input type="celphone" name="user_cel" required inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }} margin="normal"/>
 
-          <Button variant="outlined" type="submit" sx={{ mt: 2 }}>
-            Enviar
-          </Button>
-        </Box>
-      </Box>
+            <label>Email</label>
+            <input type="email" name="user_email" required/>
+            
+            <label>Mensaje</label>
+            <textarea name="message" />
+            <input type="submit" value="Enviar" required/>
+          </form>
+          </Box>
+    </Box>
     </div>
   );
+  //return (
+ //       <form
+          //component="form"
+ //         onSubmit={onSubmit}
+  //        autoComplete="off"
+  //        sx={{
+  //          display: "grid",
+  //          gridTemplateColumns: { sm: "1fr 1fr" },
+  //          gap: 2,
+  //          p: 4,
+   //       }}
+   //     >
+   //       <TextField
+   //         required
+    //        id="outlined-required"
+    //        label="Nombre y Apellido"
+    //        name="user_name"
+    //        margin="normal"
+    //      />
+     //     <TextField
+     //       required
+     //       id="outlined-required"
+     //       label="Celular"
+     //       inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+     //       margin="normal"
+     //     />
+     //     <TextField
+     //       label="Email"
+     //       name="user_email"
+     //       variant="outlined"
+     //       id="email"
+     //       type="email"
+     //       required
+     //       margin="normal"
+     //       error={error.error}
+     //       helperText={error.message}
+     //       onChange={(e) => setEmail(e.target.value)}
+     //       value={email}
+     //     />
+     //     <TextField
+     //       id="outlined-multiline-static"
+     //       label="Comentario"
+     //       name="message"
+     //       multiline
+     //       rows={4}
+     //       margin="normal"
+     //     />
+
+     //     <Button variant="outlined" type="submit" sx={{ mt: 2 }} onSubmit={sendEmail}>
+     //       Enviar
+     //     </Button>
+     //   </form>
+    //  </Box>
+   // </div>
+ // );
 }
