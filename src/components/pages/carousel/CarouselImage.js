a6ecd891345fdc4ac3fd700d3f23311721e8ab5e
ocean-girl <vpdamill@gmail.com>
2023-11-01 10:22:20 -0300

    modifico ruta de imagenes de carousel

--- src/components/pages/carousel/CarouselImage.js
+++ src/components/pages/carousel/CarouselImage.js
@@ -1,19 +1,23 @@
+import carousel1 from "../../../../public/carousel-1.jpg";
+import carousel2 from "../../../../public/carousel-2.jpg";
+import carousel3 from "../../../../public/carousel-3.jpg";
+
 export const imagesCarousel = [
   {
     id: "carousel-1",
-    img: "../../../../public/carousel-1.jpg",
+    img: carousel1,
     description:
       "En ING Consultora, nuestro compromiso firme, \n acción decidida y enfoque en resultados\n son la base de nuestro trabajo.",
   },
   {
     id: "carousel-2",
-    img: "../../../../public/carousel-2.jpg",
+    img: carousel2,
     description:
       "Nos dedicamos a la mejora continua de sus procesos,\n proporcionando soluciones a medida,\n fusionando conocimiento local con una perspectiva global.",
   },
   {
     id: "carousel-3",
-    img: "../../../../public/carousel-3.jpg",
+    img: carousel3,
     description:
       "Somos su socio estratégico a largo plazo,\n basado en la ética y la transparencia,\n comprometidos a llevar su empresa hacia el éxito sostenible.",
   },
